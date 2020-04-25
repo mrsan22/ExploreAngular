559d08046cfe7283202bb627f48b89c779b2d80d
Sanjiv Kumar <sanjiv_kumar@vrtx.com>
2020-04-25 17:05:26 -0400

    Added forms modules to see if it's included in the bundle even if not using

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   template: `
-    <p>Hello World</p>
-    <p>Hello India</p>
-    <!-- <p>Hello US</p> -->
-  `
+    <p>Hello {{ name }}</p>
+    <!-- <p>Hello India</p> -->
+    <!-- <p>Hello US</p>-->
+    <!-- <form [formGroup]="loginForm">
+      <div formGroupName="userCredentials">
+        <div class="form-group">
+          <div class="input-group">
+            <input
+              formControlName="userName"
+              class="form-control"
+              id="username"
+              placeholder="Username"
+              autofocus
+            />
+          </div>
+        </div>
+      </div>
+    </form> -->
+  `,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
+  name = "World";
+  loginForm: FormGroup;
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      userCredentials: this.formBuilder.group({
+        userName: ["", [Validators.required, Validators.minLength(2)], []],
+        password: ["", [Validators.required]],
+      }),
+    });
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
