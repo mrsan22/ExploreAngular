8a1621d6b8dc43fa4520a2dc6f3a38be6972b85f
Sanjiv Kumar <sanjiv_kumar@vrtx.com>
2020-04-25 22:47:57 -0400

    build and minify using browserify and uglifyjs

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
