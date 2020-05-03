0d11d91bf443d0faf8f15ffe5ad6d85b1334afad
Sanjiv Kumar <sanjiv_kumar@vrtx.com>
2020-05-03 01:36:31 -0400

    Added custom launch npm script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `
-    Hello World
-    <!-- <p>Hello India</p> -->
-    <!-- <p>Hello US</p>-->
-    <!-- <form [formGroup]="loginForm">
-      <div formGroupName="userCredentials">
-        <div class="form-group">
-          <div class="input-group">
-            <input
-              formControlName="userName"
-              class="form-control"
-              id="username"
-              placeholder="Username"
-              autofocus
-            />
-          </div>
-        </div>
-      </div>
-    </form> -->
-  `,
+  template: ` Hello World `,
 })
-export class AppComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
-  name = "World";
-  loginForm: FormGroup;
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      userCredentials: this.formBuilder.group({
-        userName: ["", [Validators.required, Validators.minLength(2)], []],
-        password: ["", [Validators.required]],
-      }),
-    });
-  }
+export class AppComponent {
+  constructor() {}
 }
